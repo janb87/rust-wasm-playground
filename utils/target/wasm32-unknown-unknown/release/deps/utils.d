4efd3d9c8f2b906a34c1a6cd30d1cfe3be741771
Jan Bevers <jan.bevers@kunstmaan.be>
2018-10-18 20:58:04 +0200

    Removed generated files

--- utils/target/wasm32-unknown-unknown/release/deps/utils.d
+++
@@ -1,5 +0,0 @@
-/home/projects/github/rust-wasm-playground/utils/target/wasm32-unknown-unknown/release/deps/utils.wasm: src/lib.rs
-
-/home/projects/github/rust-wasm-playground/utils/target/wasm32-unknown-unknown/release/deps/utils.d: src/lib.rs
-
-src/lib.rs:
--- utils/target/wasm32-unknown-unknown/release/utils.d
+++
@@ -1,1 +0,0 @@
-/home/projects/github/rust-wasm-playground/utils/target/wasm32-unknown-unknown/release/utils.wasm: /home/projects/github/rust-wasm-playground/utils/src/lib.rs
